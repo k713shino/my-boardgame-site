da31e3a988f2565d3cc1eaecf39adf250ccfb4cd
k713shino <shino.techno@gmail.com>
2026-03-19 10:59:51 +0900

    prisma.config.tsの改修

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "dotenv";
 
 // prisma.config.ts 評価時に .env を手動ロード
 config();
 
-const url = process.env.DATABASE_URL!;
-
 export default defineConfig({
   datasource: {
-    url,
-    adapter: new PrismaPg({ connectionString: url }),
+    url: process.env.DATABASE_URL!,
   },
   migrations: {
     seed: "npx tsx prisma/seed.ts",
